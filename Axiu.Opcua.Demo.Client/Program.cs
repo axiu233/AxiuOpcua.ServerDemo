3bffae37a2fe34ffb95041837029f7aef970a848
axiu233 <49778572+axiu233@users.noreply.github.com>
2021-10-08 16:51:10 +0800

    Update Program.cs

--- Axiu.Opcua.Demo.Client/Program.cs
+++ Axiu.Opcua.Demo.Client/Program.cs
@@ -1,4 +1,3 @@
-﻿using PECSDBInterface;
 using System;
 using System.Data.SqlClient;
 
@@ -8,8 +7,10 @@ namespace Axiu.Opcua.Demo.Client
     {
         static void Main(string[] args)
         {
-            OpcuaClientService service = new OpcuaClientService();
-            service.Execte();
+            //OpcuaClientService service = new OpcuaClientService();
+            //service.Execte();
+            Console.WriteLine("客户端仅为了编写服务端代码时做验证用,并不可使用,要验证服务端的可用性,请去网上下载其他的第三方客户端!");
+            Console.ReadKey();
         }
     }
 }
